fe6a5aba0682498614514f018a3fd03abadf0914
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-05-23 19:23:14 +0100

    Create function to (temporarily) store message from form

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("ciao");
+// Object database to store the data from the form
+const database = {};
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Create a new FormData object
+  let userMessage = new FormData(form);
+
+  userMessage.name = userMessage.get("name");
+  userMessage.email = userMessage.get("email");
+  userMessage.comment = userMessage.get("comment");
+
+  // Assign different value of the checkbox through checked property
+  const newsletter = document.getElementById("checkbox");
+  const newsletterConsent = newsletter.checked ? "on" : "off";
+  userMessage.newsletterConsent = newsletterConsent;
+
+  // Create and add to the database a unique key associated to the userMessage
+  const uniqueKey = generateUniqueKey();
+  database[uniqueKey] = userMessage;
+});
+
+// Function to generate a unique key
+function generateUniqueKey() {
+  return Math.random().toString(36).substring(7);
+}
