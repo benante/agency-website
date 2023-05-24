52f8d5d3e2ff1b5347506f3650641ee3aabdeae2
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-05-24 17:14:24 +0100

    Display flex added

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ menuButton.addEventListener("click", () => {
   menuButton.setAttribute("aria-expanded", !expanded);
   mainMenu.classList.toggle("visible");
 });
+
+// Team Section
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((wrapper) => {
+  wrapper.addEventListener("click", () => {
+    cards.forEach((wrapper) => {
+      wrapper.classList.remove("active"); // Remove "active" class from all image wrappers
+    });
+    wrapper.classList.add("active"); // Add "active" class to the clicked image wrapper
+  });
+});
