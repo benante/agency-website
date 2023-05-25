714474b8ee3e456c893ff7b55b2319e2018aa3cf
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-05-25 10:29:59 +0100

    Add javascript code to prevent default open state of hamburger menu
    Relates #19

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ menuButton.addEventListener("click", () => {
   menuButton.setAttribute("aria-expanded", !expanded);
   mainMenu.classList.toggle("visible");
 });
+
+//Prevent default open state of hamburger menu on load
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mainMenu = document.querySelector("#main-menu");
+
+  if (window.innerWidth > 700) {
+    mainMenu.classList.remove("visible");
+  } else {
+    mainMenu.style.display = "none";
+  }
+});
