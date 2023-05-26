260703b6806c76555cf53db99a0dc5255623e6e8
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-05-26 16:37:33 +0100

    fix js menu code
    Relates #14

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-//hamburger menu
+// HAMBURGER MENU
 const menuButton = document.querySelector("#hamburger");
 const mainMenu = document.querySelector("#main-menu");
 
+const logoLink = document.querySelector(".logo");
+
+mainMenu.classList.remove("visible");
+
 menuButton.addEventListener("click", () => {
+  console.log(`clicked`);
   const expanded = menuButton.getAttribute("aria-expanded") === "true" || false;
   menuButton.setAttribute("aria-expanded", !expanded);
   mainMenu.classList.toggle("visible");
-});
-
-//Prevent default open state of hamburger menu on load
-
-document.addEventListener("DOMContentLoaded", function () {
-  var mainMenu = document.querySelector("#main-menu");
-
-  if (window.innerWidth > 700) {
-    mainMenu.classList.remove("visible");
-  } else {
-    mainMenu.style.display = "none";
-  }
 });
